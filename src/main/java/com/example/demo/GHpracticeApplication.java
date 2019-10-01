6cd64f3f64a6b7c88ddf506cd0ada35c35ca637a
tanig <tanig@192.168.11.11>
2019-10-01 14:56:56 +0900

    a

---
+++ src/main/java/com/example/demo/GHpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GHpracticeApplication.class, args);
+	}
+
+}
