d26952a80155086f0c0830b56a102ecbb7d2004c
tanig <tanig@192.168.11.11>
2019-10-02 11:16:41 +0900

    管理者トップページ実装

--- src/main/java/com/example/demo/AdminTop.java
+++ src/main/java/com/example/demo/AdminTop.java
@@ -1,24 +1,27 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminTop {
 
 	//管理者画面トップページへ遷移
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String topGo(Model model) {
+	@GetMapping("/")
+	public String topGo() {
 		return "AdminTop";
 	}
 	
-	//管理者
-	@RequestMapping(value="/pList", method=RequestMethod.GET)
-	public String listGo(Model model) {
+	//管理者一覧ページへ遷移
+	@GetMapping("/aList")
+	public String listGo() {
 		return "AdminList";
 	}
 	
+	//管理者検索ページへ遷移
+	@GetMapping("/aSearch")
+	public String searchGo() {
+		return "AdminSearch";
+	}
 
 }
